22f81b427b48e763c9d5b645640bece173dc88d4
Mohammad Lahlouh <m.lahlouh@gmail.com>
2022-04-11 16:03:14 +0300

    create install
    first push
    not done

--- app/Controllers/Http/InstallsController.ts
+++ app/Controllers/Http/InstallsController.ts
@@ -8,24 +8,63 @@ import K8sClient from '@ioc:K8s/Client'
 export default class InstallsController {
   public async create({ request, response }: HttpContextContract) {
     await request.validate(CreateInstallValidator)
-    const data = K8sClient.loadYaml('01StatefulSet', { find: '{ CLIENT_NAME }', replace: 'moe' })
-    
-    return K8sClient.createStateful(data)
+    // create statefullset
+    const statefulsetYml = K8sClient.loadYaml('01StatefulSet', { find: '{ CLIENT_NAME }', replace: 'moe' })
+    return K8sClient.createStateful(statefulsetYml)
       .then(function (res) {
         // sucess code
-        response.send({
-          status: 'success',
-          message: 'Install creation request accepted',
-          debug: res.body,
-        })
+        // create service
+        const serviceYml = K8sClient.loadYaml('02Service', { find: '{ CLIENT_NAME }', replace: 'moe' })
+        return K8sClient.createService(serviceYml)
+          .then(function (res) {
+            // success code
+            // create certificate
+            const certificateYml = K8sClient.loadYaml('03Certificate', { find: '{ CLIENT_NAME }', replace: 'moe' })
+            return K8sClient.createCertificate(certificateYml)
+              .then(function (res) {
+                // success code
+                // create ingress
+                const ingressYml = K8sClient.loadYaml('04Ingress', { find: '{ CLIENT_NAME }', replace: 'moe' })
+                return K8sClient.createIngress(ingressYml)
+                  .then(function (res) {
+                    response.send({
+                      status: 'success',
+                      message: 'Install creation request accepted',
+                      debug: res.body,
+                    })
+                  })
+                  .catch(function (err) {
+                    response.status(403).send({
+                      status: 'createIngress Failed',
+                      message: err.message,
+                      debug: err.body,
+                    })
+                  })
+              })
+              .catch(function (err) {
+                response.status(403).send({
+                  status: 'createCertificate Failed',
+                  message: err.message,
+                  debug: err.body,
+                })
+              })
+          })
+          .catch(function (err) {
+            response.status(403).send({
+              status: 'createService Failes',
+              message: err.message,
+              debug: err.body,
+            })
+          })
       })
       .catch(function (err) {
-          response.status(403).send({
-          status: '1234567',
+        response.status(403).send({
+          status: 'createStateful Failes',
           message: err.message,
           debug: err.body,
         })
       })
+
   }
 
   public async update({ request, response }: HttpContextContract) {
--- providers/K8sProvider/K8s.ts
+++ providers/K8sProvider/K8s.ts
@@ -1,4 +1,4 @@
-import { AppsV1Api, KubeConfig, V1StatefulSet } from '@kubernetes/client-node'
+import { AppsV1Api, KubeConfig, V1StatefulSet, CoreV1Api, CustomObjectsApi, ExtensionsV1beta1Api } from '@kubernetes/client-node'
 import { types } from '@ioc:Adonis/Core/Helpers'
 import { join } from 'path'
 import { readFileSync } from 'fs-extra'
@@ -7,13 +7,19 @@ import { extend } from 'lodash'
 import { K8sConfig } from 'Config/k8s'
 
 export default class K8sWrapper {
-  protected apiClient: AppsV1Api
+  protected AppsV1Api: AppsV1Api
+  protected CoreV1Api: CoreV1Api
+  protected CustomObjectsApi: CustomObjectsApi
+  protected ExtensionsV1beta1Api: ExtensionsV1beta1Api
 
   constructor(config: typeof K8sConfig) {
     const kc = new KubeConfig()
     kc.loadFromOptions(config)
 
-    this.apiClient = kc.makeApiClient(AppsV1Api)
+    this.AppsV1Api = kc.makeApiClient(AppsV1Api)
+    this.CoreV1Api = kc.makeApiClient(CoreV1Api)
+    this.CustomObjectsApi = kc.makeApiClient(CustomObjectsApi)
+    //this.ExtensionsV1beta1Api = kc.makeApiClient(ExtensionsV1beta1Api)
   }
 
   public loadYaml(fileName: String, replace?: Object): Object {
@@ -32,10 +38,28 @@ export default class K8sWrapper {
   public createStateful(data: Object) {
     const state = new V1StatefulSet()
     extend(state, data)
-    return this.apiClient.createNamespacedStatefulSet('default', state)
+    return this.AppsV1Api.createNamespacedStatefulSet('default', state)
+  }
+
+  public createService(data: Object) {
+    const state = new V1StatefulSet()
+    extend(state, data)
+    return this.CoreV1Api.createNamespacedService('default', state)
+  }
+
+  public createCertificate(data: Object) {
+    const state = new V1StatefulSet()
+    extend(state, data)
+    return this.CustomObjectsApi.createNamespacedCustomObject("cert-manager.io", "v1", "default", "certificates", state)
+  }
+
+  public createIngress(data: Object) {
+    const state = new V1StatefulSet()
+    extend(state, data)
+    return this.ExtensionsV1beta1Api.createNamespacedIngress('default', state)
   }
 
   public getClient() {
-    return this.apiClient
+    return this.AppsV1Api
   }
 }
