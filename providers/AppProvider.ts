57b0913885505e5bcecf776233f390b81db70fb4
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-05-13 17:19:56 +0300

    better envs

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -10,43 +10,14 @@ export default class AppProvider {
   public async boot() {
     const HealthCheck = this.app.container.use('Adonis/Core/HealthCheck')
 
-    HealthCheck.addChecker('token', async () => {
-      const token = Env.get('TOKEN')
-      return {
-        displayName: 'Token Check',
-        health: {
-          healthy: token ? true : false,
-        },
-      }
-    })
-
-    HealthCheck.addChecker('k8s_api', async () => {
-      const K8S_API_URL = Env.get('K8S_API_URL')
-      return {
-        displayName: 'K8S_API_URL Check',
-        health: {
-          healthy: K8S_API_URL ? true : false,
-        },
-      }
-    })
-
     HealthCheck.addChecker('k8s_api_can_connect', async () => {
       const K8S_API_URL = Env.get('K8S_API_URL')
       const canReach = await isReachable(K8S_API_URL)
-      if (canReach) {
-        return {
-          displayName: 'K8s can connect Check',
-          health: {
-            healthy: true,
-          },
-        }
-      } else {
-        return {
-          displayName: 'K8s can connect Check',
-          health: {
-            healthy: false,
-          },
-        }
+      return {
+        displayName: 'K8s can connect Check',
+        health: {
+          healthy: canReach,
+        },
       }
     })
   }
