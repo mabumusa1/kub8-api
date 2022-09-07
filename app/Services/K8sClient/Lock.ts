e40ac708f6192ac13e27a6b67dec5a03464160ed
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-09-07 08:33:54 +0300

    fix lint

--- app/Services/K8sClient/Lock.ts
+++ app/Services/K8sClient/Lock.ts
@@ -1,19 +1,24 @@
-import { CoreV1Api, KubeConfig, V1Secret, NetworkingV1Api, V1Ingress } from '@kubernetes/client-node'
+import {
+  CoreV1Api,
+  KubeConfig,
+  V1Secret,
+  NetworkingV1Api,
+  V1Ingress,
+} from '@kubernetes/client-node'
 import { extend } from 'lodash'
 import K8sErrorException from 'App/Exceptions/K8sErrorException'
 import crypto from 'crypto'
 import { base64 } from '@ioc:Adonis/Core/Helpers'
 
 export class Lock {
-    protected CoreV1ApiClient: CoreV1Api
-    protected NetworkingV1ApiClient: NetworkingV1Api
+  protected CoreV1ApiClient: CoreV1Api
+  protected NetworkingV1ApiClient: NetworkingV1Api
 
-    constructor(kc: KubeConfig) {
-        this.CoreV1ApiClient = kc.makeApiClient(CoreV1Api)
-        this.NetworkingV1ApiClient = kc.makeApiClient(NetworkingV1Api)
-      }
+  constructor(kc: KubeConfig) {
+    this.CoreV1ApiClient = kc.makeApiClient(CoreV1Api)
+    this.NetworkingV1ApiClient = kc.makeApiClient(NetworkingV1Api)
+  }
 
-      
   /**
    * Create a Ingress based on the yaml file passed
    *
@@ -30,28 +35,35 @@ export class Lock {
         return true
       })
       .catch((err) => {
-          console.log(err.body)
+        console.log(err.body)
         throw new K8sErrorException('Error Creating Secret ' + err.body)
       })
   }
 
-  public async attachLock(resourceName: string, data: Object){
+  public async attachLock(resourceName: string, data: Object) {
     const state = new V1Ingress()
     extend(state, data)
     console.log(data)
     console.log(state)
-    return await this.NetworkingV1ApiClient.patchNamespacedIngress(resourceName, 'default', 'false', 'false', 'StrategicMergePatch', true, 
-      '{ headers: {"Content-Type": "application/strategic-merge-patch+json"} } ')
-    .then(() => {
+    return await this.NetworkingV1ApiClient.patchNamespacedIngress(
+      resourceName,
+      'default',
+      'false',
+      'false',
+      'StrategicMergePatch',
+      true,
+      '{ headers: {"Content-Type": "application/strategic-merge-patch+json"} } '
+    )
+      .then(() => {
         return true
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err)
         throw new K8sErrorException('Error Attaching Lock ' + err.body)
-    })
+      })
   }
 
-  public createHash(password: string) : string{
+  public createHash(password: string): string {
     let hash = crypto.createHash('sha1')
     hash.update(password)
     const hashedPassed = '{SHA}' + hash.digest('base64')
